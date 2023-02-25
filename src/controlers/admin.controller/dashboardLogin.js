fa7d0f99b0402a83f768b1c4e44251d42698d1de
Tiron Raul <raul.tiron@smartboxdigital.com>
2023-02-25 18:45:20 +0200

    done with the admin login

--- src/controlers/admin.controller/dashboardLogin.js
+++ src/controlers/admin.controller/dashboardLogin.js
@@ -1,9 +1,47 @@
 import Admin from "../../models/admin.model";
 
 const dashboardLogin = async (req, res, _next) => {
+    try {
+        const { userName, password } = req.body;
 
+        if (typeof userName === 'undefined' || typeof password === 'undefined') {
+          return res.status(400).json({
+            error: 'Username and Password required'
+          });
+        }
     
-
+        if (typeof userName !== 'string' || typeof password !== 'string') {
+          return res.status(400).json({
+            error: 'Username and Password must be of string type'
+          });
+        }
+    
+        const admin = await Admin.findOne({ userName: userName });
+    
+        if (!admin) {
+          return res.status(404).json({
+            error: 'User does not exist'
+          });
+        }
+    
+        if (admin.password !== password) {
+          return res.status(400).json({
+            error: 'Invalid Password'
+          });
+        }
+    
+        admin.loggedIn = true;
+        
+        return res.status(200).json({
+          message: 'Successfully logged in',
+          adminUser: admin
+        });
+        
+    } catch (error) {
+        return res.status(500).json({
+            error: error
+        });
+    }
 };
 
 export default dashboardLogin;
