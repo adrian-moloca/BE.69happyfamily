fca0d22e327a9de913b445acb6d53ac5b669ba93
vlad-vararian <vlad.vararian@smartboxdigital.com>
2023-02-28 18:31:25 +0200

    updated to register with only certains email, but throws error on save

--- src/controlers/admin.controller/dashboardRegister.js
+++ src/controlers/admin.controller/dashboardRegister.js
@@ -2,90 +2,99 @@ import Admin from "../../models/admin.model.js";
 import validateBodyRequest from "../../utils/validateBodyRequest.js";
 
 const dashboardRegister = async (req, res, _next) => {
-    try {
-      const { isUndefined, message } = validateBodyRequest(req.body);
-      const { userName, password, firstName, lastName, email, age } = req.body;
+  try {
+    const { isUndefined, message } = validateBodyRequest(req.body);
+    const { userName, password, firstName, lastName, email, age } = req.body;
+    const adminEmails = process.env.ADMIN_EMAILS.split(",");
 
-      if(isUndefined === true){
-        return res.status(400).json({message: message});
-      }
-
-    if (typeof userName !== 'string' ||
-        typeof password !== 'string' ||
-        typeof age !== 'number' ||
-        typeof firstName !== 'string' ||
-        typeof lastName !== 'string' ||
-        typeof email !== 'string') {
-        return res.status(400).json({
-          error: 'Invalid type'
-        });
-      }
-
-      if (password.length < 8) {
-        return res.status(400).json({
-          error: 'Password must be at least 8 characters'
-        });
-      }
-  
-      if (userName.length < 3) {
-        return res.status(400).json({
-          error: 'Username must be at least 3 characters'
-        });
-      }
-  
-      if(age < 18){
-        return res.status(400).json({
-          error: "Too young to use our platform"
-        })
-      }
-
-        const whitelistedEmail = process.env.WHITELIST.split(',').map((email) => {
-          if (condition){}
-        })
-        console.log(whitelistedEmail);
-
-        const adminEmailExists = await Admin.findOne({email: email});
-
-        if (adminEmailExists){
-          return res.status(409).json({
-            error: "Conflict. Admin user with this email already exists"
-          });
-        };
-
-      const adminExists = await Admin.findOne({ userName: userName });
-
-      if (adminExists) {
-        return res.status(409).json({
-          error: 'Admin user already exists'
-        });
-      }
-  
-      const newAdminUser = new Admin({
-        userName,
-        password,
-        age,
-        email,
-        firstName,
-        lastName
-      });
-  
-      await newAdminUser
-        .save()
-        .then((adminUser) => {
-            adminUser.loggedIn = true;
-          return res.status(201).json({
-            message: 'Admin User Created!',
-            adminUser
-          });
-        })
-        .catch((err) => {
-        return res.status(500).json({
-          err: err
-        });
-        });
-    } catch (error) {
-      console.error(error);
+    if (isUndefined === true) {
+      return res.status(400).json({ message: message });
     }
+
+    if (
+      typeof userName !== "string" ||
+      typeof password !== "string" ||
+      typeof age !== "number" ||
+      typeof firstName !== "string" ||
+      typeof lastName !== "string" ||
+      typeof email !== "string"
+    ) {
+      return res.status(400).json({
+        error: "Invalid type",
+      });
+    }
+
+    if (!adminEmails.includes(email)) {
+      return res.status(400).json({
+        error: "You can't create an account with this email",
+      });
+    }
+
+    if (password.length < 8) {
+      return res.status(400).json({
+        error: "Password must be at least 8 characters",
+      });
+    }
+
+    if (userName.length < 3) {
+      return res.status(400).json({
+        error: "Username must be at least 3 characters",
+      });
+    }
+
+    if (age < 18) {
+      return res.status(400).json({
+        error: "Too young to use our platform",
+      });
+    }
+
+    const adminEmailExists = await Admin.findOne({ email: email });
+
+    if (adminEmailExists) {
+      return res.status(409).json({
+        error: "Conflict. Admin user with this email already exists",
+      });
+    }
+
+    const adminExists = await Admin.findOne({ userName: userName });
+
+    if (adminExists) {
+      return res.status(409).json({
+        error: "Admin user already exists",
+      });
+    }
+    const admins = await Admin.find();
+    console.log(admins);
+    const newAdminUser = new Admin({
+      userName,
+      password,
+      age,
+      email,
+      firstName,
+      lastName,
+    });
+
+    await newAdminUser
+      .save()
+      .then((adminUser) => {
+        adminUser.loggedIn = true;
+        return res.status(201).json({
+          message: "Admin User Created!",
+          adminUser,
+        });
+      })
+      .catch((err) => {
+        return res.status(500).json({
+          message: "Save error",
+          error: err,
+        });
+      });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Server error",
+      error: error,
+    });
+  }
 };
 
 export default dashboardRegister;
--- src/models/admin.model.js
+++ src/models/admin.model.js
@@ -1,18 +1,18 @@
 import mongoose from "mongoose";
 
-const adminModel = new mongoose.Schema({
-
+const adminModel = new mongoose.Schema(
+  {
     userName: { type: String, required: true },
     password: { type: String, required: true },
-    email: { type: String, required: true},
-    age: { type: Number, required: true},
+    email: { type: String, required: true },
+    age: { type: Number, required: true },
     firstName: { type: String, required: true },
-    lastName: { type: String, required: true},
-    loggedIn: { type: Boolean, default: false}
-
-},
-{
-    timestamps: true
-});
+    lastName: { type: String, required: true },
+    loggedIn: { type: Boolean, default: false },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export default mongoose.model("Admin", adminModel);
