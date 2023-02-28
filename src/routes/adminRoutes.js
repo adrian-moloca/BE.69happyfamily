8e933b9b34c46a544953b48f982599ff4817c984
vlad-vararian <vlad.vararian@smartboxdigital.com>
2023-02-28 19:41:58 +0200

    added dashboard login route to the admin routes

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,8 +1,10 @@
-import express  from "express";
+import express from "express";
+import dashboardLogin from "../controlers/admin.controller/dashboardLogin.js";
 import dashboardRegister from "../controlers/admin.controller/dashboardRegister.js";
 
 const adminRoutes = express.Router();
 
-adminRoutes.post('/register', dashboardRegister);
+adminRoutes.post("/register", dashboardRegister);
+adminRoutes.post("/login", dashboardLogin);
 
 export default adminRoutes;
