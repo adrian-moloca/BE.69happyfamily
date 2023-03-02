1b0152ff6c703cca0d8109181f05956166bd6fad
vlad-vararian <vlad.vararian@smartboxdigital.com>
2023-03-02 18:25:44 +0200

    updated with bcrypt on admin login

--- src/controlers/admin.controller/dashboardLogin.js
+++ src/controlers/admin.controller/dashboardLogin.js
@@ -1,46 +1,47 @@
 import Admin from "../../models/admin.model.js";
+import bcrypt from "bcrypt";
 
 const dashboardLogin = async (req, res, _next) => {
   const { userName, password } = req.body;
-    try {
-        if (typeof userName === 'undefined' || typeof password === 'undefined') {
-          return res.status(400).json({
-            error: 'Username and Password required'
-          });
-        }
-    
-        if (typeof userName !== 'string' || typeof password !== 'string') {
-          return res.status(400).json({
-            error: 'Username and Password must be of string type'
-          });
-        }
-    
-        const admin = await Admin.findOne({ userName: userName });
-    
-        if (!admin) {
-          return res.status(404).json({
-            error: 'User does not exist'
-          });
-        }
-    
-        if (admin.password !== password) {
-          return res.status(400).json({
-            error: 'Invalid Password'
-          });
-        }
-    
-        admin.loggedIn = true;
-        
-        return res.status(200).json({
-          message: 'Successfully logged in',
-          adminUser: admin
-        });
-        
-    } catch (error) {
-        return res.status(500).json({
-            error: error
-        });
+  try {
+    if (typeof userName === "undefined" || typeof password === "undefined") {
+      return res.status(400).json({
+        error: "Username and Password required",
+      });
     }
+
+    if (typeof userName !== "string" || typeof password !== "string") {
+      return res.status(400).json({
+        error: "Username and Password must be of string type",
+      });
+    }
+
+    const admin = await Admin.findOne({ userName: userName });
+
+    if (!admin) {
+      return res.status(404).json({
+        error: "User does not exist",
+      });
+    }
+
+    const validPassword = await bcrypt.compare(password, admin.password);
+    if (!validPassword) {
+      return res.status(400).json({
+        error: "Invalid password.",
+      });
+    }
+
+    admin.loggedIn = true;
+
+    return res.status(200).json({
+      message: "Successfully logged in",
+      adminUser: admin,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: error,
+    });
+  }
 };
 
 export default dashboardLogin;
