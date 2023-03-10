dd14e463c135f6efa328212e8a1acb5fe5af9b45
tiron-raul <raul.tiron@smartboxdigital.com>
2023-03-10 16:00:52 +0200

    judfeijwjdfweu

--- src/utils/validators/validateBodyRequest.js
+++ src/utils/validators/validateBodyRequest.js
@@ -1,6 +1,6 @@
 const validateBodyRequest = (bodyRequestObj) => {
-    console.log(bodyRequestObj);
-    console.log(Object.entries(bodyRequestObj));
+    // console.log(bodyRequestObj);
+    // console.log(Object.entries(bodyRequestObj));
     const data = Object.entries(bodyRequestObj);
     let isUndefined = false;
     let message = '';
@@ -9,7 +9,7 @@ const validateBodyRequest = (bodyRequestObj) => {
         if(data[i][1] === ''){
             message = `${data[i][0]} cannot be undefined`;
             isUndefined = true;
-            console.log(data[i][0]);
+            // console.log(data[i][0]);
             break;
         }
     }
